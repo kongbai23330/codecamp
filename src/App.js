aa33b3b4e8764faf527811e197d19719a7e8a0e8
kongbai23330 <109857471+kongbai23330@users.noreply.github.com>
2024-01-07 14:40:55 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -6,19 +6,44 @@ import Post from './component/post';
 import SearchBar from './component/SearchBar';
 import VerticalSidebar from './component/VerticalSidebar';
 import { Box, Typography } from '@mui/material';
-
+import SecondPage from './component/SecondPage';
+import UserInfo from './component/UserInfo';
 
 function App() {
   return (
     <Router>
       <Box sx={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
         {/* Header area with title and search bar */}
-        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', p: 2, bgcolor: 'white' }}>
-          <Typography variant="h4" sx={{ color: '#033375', position: 'absolute', left: '10px' }}>
-            TravelFinland
-          </Typography>
-          <SearchBar /> {/* Centered search bar */}
-        </Box>
+        <Box sx={{ 
+  display: 'flex', 
+  justifyContent: 'space-between', // This will push the children to the edges
+  alignItems: 'center', 
+  p: 2, 
+  bgcolor: 'white' 
+}}>
+  <Typography 
+    variant="h4" 
+    sx={{ 
+      color: '#033375', 
+      marginLeft: '10px' // Use margin instead of absolute positioning
+    }}
+  >
+    TravelFinland
+  </Typography>
+  <Box sx={{ 
+    flexGrow: 1, 
+    display: 'flex', 
+    justifyContent: 'center' // Center the search bar in the available space
+  }}>
+    <SearchBar />
+  </Box>
+  <Box sx={{ 
+    flexGrow: 0 // Ensure UserInfo doesn't grow and stays on the right
+  }}>
+    <UserInfo />
+  </Box>
+</Box>
+
 
         {/* Main content area */}
         <Box sx={{ display: 'flex', flexGrow: 1 }}>
@@ -28,6 +53,7 @@ function App() {
           <Box sx={{ flexGrow: 1 }}>
             <Routes>
               <Route exact path="/" element={<Page />} />
+              <Route exact path="/SecondPage" element={<SecondPage />} />
               <Route path="/post/:id" element={<Post />} />
               {/* Add more routes as needed */}
             </Routes>
