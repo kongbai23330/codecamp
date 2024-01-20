9172fc1188216e07f01792d71d54b25fa62c7aea
kongbai23330 <109857471+kongbai23330@users.noreply.github.com>
2024-01-20 17:58:28 +0200

    Add files via upload

--- src/component/Page.js
+++ src/component/Page.js
@@ -1,43 +1,66 @@
 // Page.js
-import React from 'react'
-import ImageCard from './ImageCard'
-import { Box, styled } from '@mui/material'
+import React from 'react';
+import ImageCard from './ImageCard';
+import { Box, styled } from '@mui/material';
 
 class Page extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       posts: [],
-    }
+      page: 0,
+      hasMore: true,
+    };
   }
 
   componentDidMount() {
-    this.fetchPosts()
+    this.fetchPosts();
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
   }
 
   fetchPosts = async () => {
-    try {
-      const response = await fetch('http://localhost:1234/posts', {
-        method: 'get',
-      })
-      if (!response.ok) {
-        throw new Error('Network response was not ok')
+    const { page, posts } = this.state;
+
+    if (this.state.hasMore) {
+      try {
+        const response = await fetch(`http://localhost:1234/posts?page=${page}&limit=20`, { method: 'get' });
+
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+
+        const newPosts = await response.json();
+        this.setState({
+          posts: [...posts, ...newPosts],
+          page: page + 1,
+          hasMore: newPosts.length === 20, // assuming 20 is the limit per page
+        });
+      } catch (error) {
+        console.error('Could not fetch posts:', error);
       }
-      const data = await response.json()
-      this.setState({ posts: data })
-    } catch (error) {
-      console.error('Could not fetch posts: ', error)
     }
-  }
+  };
+
+  handleScroll = () => {
+    const { innerHeight, scrollY } = window;
+    const { offsetHeight } = document.documentElement;
+
+    if (innerHeight + scrollY >= offsetHeight - 500) { // Fetch more posts a bit before reaching the bottom
+      this.fetchPosts();
+    }
+  };
 
   render() {
-    const { posts } = this.state
-    const { searchVal = '' } = this.props // Assuming searchVal is passed as a prop
+    const { posts } = this.state;
 
     const ImageCardStyled = styled(ImageCard)({
       maxWidth: '250px',
       height: 'auto',
-    })
+    });
 
     return (
       <Box sx={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
@@ -50,32 +73,20 @@ class Page extends React.Component {
                 gap: 2,
               }}
             >
-              {posts
-                .filter((post) =>
-                  post.title.toLowerCase().includes(searchVal.toLowerCase()),
-                )
-                .map((post) => (
-                  // post.images.map((image, index) => (
-                  //   <ImageCardStyled
-                  //     key={`${post._id}_${index}`}
-                  //     src={`data:image/jpeg;base64,${post.images[index]}`}
-                  //     label={post.title}
-                  //     id={post._id}
-                  //   />
-                  // )),
-                  <ImageCardStyled
-                    key={`${post._id}`}
-                    src={`data:image/jpeg;base64,${post.images[0]}`}
-                    label={post.title}
-                    id={post._id}
-                  />
-                ))}
+              {posts.map((post) => (
+                <ImageCardStyled
+                  key={post._id}
+                  src={`data:image/jpeg;base64,${post.images[0]}`}
+                  label={post.title}
+                  id={post._id}
+                />
+              ))}
             </Box>
           </Box>
         </Box>
       </Box>
-    )
+    );
   }
 }
 
-export default Page
+export default Page;
--- src/component/SearchBar.js
+++ src/component/SearchBar.js
@@ -1,29 +1,44 @@
 // SearchBar.js
-import * as React from 'react'
-import SearchIcon from '@mui/icons-material/Search'
-import { InputBase, Paper } from '@mui/material'
+import * as React from 'react';
+import SearchIcon from '@mui/icons-material/Search';
+import { InputBase, Paper } from '@mui/material';
+
+// SearchBar.js
+// ... other imports ...
 
 class SearchBar extends React.Component {
+  state = {
+    searchQuery: '',
+  };
+
   onChange = (e) => {
-    this.props.updateSearch(e.target.value)
-  }
+    this.setState({ searchQuery: e.target.value });
+  };
+
+  onSubmit = async (e) => {
+    e.preventDefault();
+    this.props.onSearch(this.state.searchQuery); // Lift up the state
+  };
 
   render() {
     return (
       <Paper
         component="form"
         sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
+        onSubmit={this.onSubmit}
       >
         <InputBase
           sx={{ ml: 1, flex: 1 }}
           placeholder="Search what you want"
           inputProps={{ 'aria-label': 'search what you want' }}
+          value={this.state.searchQuery}
           onChange={this.onChange}
         />
         <SearchIcon sx={{ p: '10px' }} />
       </Paper>
-    )
+    );
   }
 }
 
-export default SearchBar
+export default SearchBar;
+
